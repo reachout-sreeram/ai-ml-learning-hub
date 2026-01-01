421bf63fc9988d5eff4cfaa7d3fe7327b0e3b8dd
Saketh sreeram <reachout.sreeram@gmail.com>
2026-01-01 09:31:51 +0530

    feat: add glossary definition for 'Supervised Learning'

---
+++ src/data/glossaryData.js
@@ -0,0 +1,8 @@
+// Automatically generated AI/ML Glossary data (index: 1)
+export const glossary = [
+  {
+    "term": "Supervised Learning",
+    "definition": "A machine learning paradigm where the model is trained on input-output pairs.",
+    "category": "General ML"
+  }
+];

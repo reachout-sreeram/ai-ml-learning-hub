652f44b3b301c7bbbbb0b8e80547791a17c5853a
Saketh sreeram <reachout.sreeram@gmail.com>
2026-01-07 09:48:14 +0530

    docs: refine definition details for 'Support Vector Machine'

--- src/data/glossaryData.js
+++ src/data/glossaryData.js
@@ -1,4 +1,4 @@
-// Automatically generated AI/ML Glossary data (index: 225)
+// Automatically generated AI/ML Glossary data (index: 226)
 export const glossary = [
   {
     "term": "Supervised Learning",

3c3f43275bb9134ca65ce1983848c54845ba75b5
Saketh sreeram <reachout.sreeram@gmail.com>
2026-01-07 11:35:14 +0530

    docs: refine definition details for 'K-Means Clustering'

--- src/data/glossaryData.js
+++ src/data/glossaryData.js
@@ -1,4 +1,4 @@
-// Automatically generated AI/ML Glossary data (index: 233)
+// Automatically generated AI/ML Glossary data (index: 234)
 export const glossary = [
   {
     "term": "Supervised Learning",

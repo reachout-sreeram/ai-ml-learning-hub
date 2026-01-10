98c89b253cd4457ee9249437207cee0efaaab35d
Saketh sreeram <reachout.sreeram@gmail.com>
2026-01-10 14:44:07 +0530

    docs: refine definition details for 'Few-shot Learning'

--- src/data/glossaryData.js
+++ src/data/glossaryData.js
@@ -1,4 +1,4 @@
-// Automatically generated AI/ML Glossary data (index: 371)
+// Automatically generated AI/ML Glossary data (index: 372)
 export const glossary = [
   {
     "term": "Supervised Learning",

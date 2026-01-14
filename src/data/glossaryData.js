75e114b3831e5fa30f7e6bd82c54643f26f84401
Saketh sreeram <reachout.sreeram@gmail.com>
2026-01-14 16:15:05 +0530

    docs: refine definition details for 'Mixture of Experts'

--- src/data/glossaryData.js
+++ src/data/glossaryData.js
@@ -1,4 +1,4 @@
-// Automatically generated AI/ML Glossary data (index: 534)
+// Automatically generated AI/ML Glossary data (index: 535)
 export const glossary = [
   {
     "term": "Supervised Learning",

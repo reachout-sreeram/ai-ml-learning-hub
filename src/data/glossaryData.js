e19b6748fccbdfffdd859823c4d9312e3006c843
Saketh sreeram <reachout.sreeram@gmail.com>
2026-01-14 10:16:41 +0530

    docs: refine definition details for 'Quantization'

--- src/data/glossaryData.js
+++ src/data/glossaryData.js
@@ -1,4 +1,4 @@
-// Automatically generated AI/ML Glossary data (index: 509)
+// Automatically generated AI/ML Glossary data (index: 510)
 export const glossary = [
   {
     "term": "Supervised Learning",

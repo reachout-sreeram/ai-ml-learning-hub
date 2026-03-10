f95219d8e2216c6657561f49cc551939de677d4d
Saketh sreeram <reachout.sreeram@gmail.com>
2026-03-10 16:40:32 +0530

    docs: fix active tab link border highlights

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -117,4 +117,4 @@ const Header = ({ activeTab, setActiveTab }) => {
 
 export default Header
 
-// Tracker: index=106, date=2026-03-09 10:11:31 +0530
+// Tracker: index=124, date=2026-03-10 16:40:32 +0530

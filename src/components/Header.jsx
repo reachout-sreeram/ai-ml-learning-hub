9b9c9e4027df7af58882c7778a27525da2021e7c
Saketh sreeram <reachout.sreeram@gmail.com>
2026-03-17 09:39:17 +0530

    Cleaned up Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -117,4 +117,4 @@ const Header = ({ activeTab, setActiveTab }) => {
 
 export default Header
 
-// Tracker: index=186, date=2026-03-15 11:30:07 +0530
+// Tracker: index=209, date=2026-03-17 09:39:17 +0530
